ae6b018dbb2bbc8baef76fc821c8b1cb2b321be9
luca-scharr <73334021+luca-scharr@users.noreply.github.com>
2022-07-26 08:56:15 +0200

    Fixed training procedure

--- LTH_and_language_transformer.py
+++ LTH_and_language_transformer.py
@@ -219,14 +219,14 @@ def train(epoch: int) -> float:
                 grad_tensor = np.where(tensor < epsilon, 0, grad_tensor)
                 p.grad.data = T.from_numpy(grad_tensor).to(device)
         # Clipping Gradients
-        T.nn.utils.clip_grad_norm_(model.parameters(), 5.)
+        T.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
         optimizer.step()
         total_loss += t_loss.item()
         if batch % log_interval == 0 and batch > 0:
-            comp_loss    = total_loss
             ms_per_batch = (time.time() - start_time) * 1000 / log_interval
             cur_loss     = (total_loss - comp_loss) / log_interval
             # ppl          = math.exp(cur_loss)
+            comp_loss = total_loss
             print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
                   f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
                   f'loss {cur_loss:5.2f}')  # | ppl {ppl:8.2f}')
